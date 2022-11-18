d515dceed41cb6ceb4dc7f306fcaf6c2c6619285
96Akhil <115717620+96Akhil@users.noreply.github.com>
2022-11-18 11:38:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,208 @@
+let username = document.getElementById("username");
+let phone = document.getElementById("phone");
+let email = document.getElementById("email");
+let pwd = document.getElementById("pwd");
+let usernameerror = document.getElementById("usernameerror");
+let phoneerror = document.getElementById("phoneerror");
+let emailerror = document.getElementById("emailerror");
+let pwderror = document.getElementById("pwderror");
+let error = document.getElementsByClassName("error")
+let strengthbar = document.getElementById("strength-bar");
+let msg = document.getElementById("msg"); 
+let loginemail = document.getElementById("loginemail");
+let loginpwd = document.getElementById("loginpwd");
+let loginemailerror = document.getElementById("loginemailerror");
+let loginpwderror = document.getElementById("loginpwderror");
+
+
+
+function validateName() {
+  
+  let regexpname = /^([a-zA-Z ]{3,})$/;
+
+  if (regexpname.test(username.value)) {
+    usernameerror.innerHTML = "Valid";
+    usernameerror.style.color = "Green";
+    username.style.border = "2px solid green";
+    return true;
+  } else if (username.value.trim() == "") {
+    usernameerror.innerHTML = "Name field is empty";
+    usernameerror.style.color = "Red";
+    username.style.border = "2px solid red";
+    return false;
+  } else {
+    usernameerror.innerHTML = "Invalid name";
+    usernameerror.style.color = "Red";
+    username.style.border = "2px solid red";
+    return false;
+  }
+}
+  function validatePhone(){
+
+    let regexpphone = /^[789][0-9]{9}$/;
+
+  if (regexpphone.test(phone.value)) {
+    phoneerror.innerHTML = "Valid";
+    phoneerror.style.color = "Green";
+    phone.style.border = "2px solid green";
+    return true;
+  } else if (phone.value.trim() == "") {
+    phoneerror.innerHTML = "Phone no. field is empty";
+    phoneerror.style.color = "Red";
+    phone.style.border = "2px solid red";
+    return false;
+  } else {
+    phoneerror.innerHTML = "Invalid Phone no.";
+    phoneerror.style.color = "Red";
+    phone.style.border = "2px solid red";
+    return false;
+  }
+}
+
+function validateEmail(){
+
+  let regexpemail =
+  /^([A-Za-z0-9_\-\.]{3,})@([A-Za-z]{3,})\.([A-Za-z]{2,5})(\.[A-za-z]{2,5})?$/;
+
+  if (regexpemail.test(email.value)) {
+    emailerror.innerHTML = "Valid";
+    emailerror.style.color = "Green";
+    email.style.border = "2px solid Green";
+    return true;
+  } else if (email.value.trim() == "") {
+    emailerror.innerHTML = "Email field is empty";
+    emailerror.style.color = "Red";
+    email.style.border = "2px solid red";
+    return false;
+  } else {
+    emailerror.innerHTML = "Invalid email";
+    emailerror.style.color = "Red";
+    email.style.border = "2px solid red";
+    return false;
+  }
+
+}
+
+
+function validatepwd(){
+
+  let regexppwd = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{1,16}$/;
+
+  if (regexppwd.test(pwd.value)) {
+    pwderror.innerHTML = "Valid";
+    pwderror.style.color = "green";
+    pwd.style.border = "2px solid green";
+    return true;
+  } else if (pwd.value.trim() == "") {
+    pwderror.innerHTML = "Password field is empty";
+    pwderror.style.color = "Red";
+    pwd.style.border = "2px solid red";
+    return false;
+  } else {
+    pwderror.innerHTML = "Invalid Password";
+    pwderror.style.color = "Red";
+    pwd.style.border = "2px solid red";
+    return false;
+  }
+
+}
+
+function strength(){
+    let regweak=/^[a-zA-Z]{0,}$/
+    let regmed = /^[a-zA-Z0-9]{3,}$/
+    let regstrong = /^[a-zA-Z0-9!@#$%^&*]{6,}$/
+  
+    if(regweak.test(pwd.value)){
+    
+      msg.innerHTML = "Weak Password";
+      msg.style.color = "Red"
+      strengthbar.style.backgroundColor = "Red";
+    }
+  
+    else if(regmed.test(pwd.value)){
+      msg.innerHTML = "Medium Password"
+      msg.style.color= "Orange"
+      strengthbar.style.backgroundColor="Orange";
+    }
+  
+    else if(regstrong.test(pwd.value)){
+      msg.innerHTML = "Strong Password"
+      msg.style.color = "Green"
+      strengthbar.style.backgroundColor="Green";
+    }
+  
+  }
+
+function validate(){
+
+  if ((validateName()==true)&&(validateEmail()==true)&&(validatePhone()==true)&&(validatepwd()==true)) {
+    alert("Form submitted successfully!")
+    location.reload();
+  }
+  else {
+    alert("Form not submitted, please check the empty fields!!")
+  }
+}
+
+
+function loginValidateemail(){
+
+    let regloginemail =
+    /^([A-Za-z0-9_\-\.]{3,})@([A-Za-z]{3,})\.([A-Za-z]{2,5})(\.[A-za-z]{2,5})?$/;
+
+    if (regloginemail.test(loginemail.value)) {
+        loginemailerror.innerHTML = "Valid";
+        loginemailerror.style.color = "Green";
+        loginemail.style.border = "2px solid Green";
+        return true;
+      } else if (loginemail.value.trim() == "") {
+        loginemailerror.innerHTML = "Email field is empty";
+        loginemailerror.style.color = "Red";
+        loginemail.style.border = "2px solid red";
+        return false;
+      } else {
+        loginemailerror.innerHTML = "Enter a valid email-ID";
+        loginemailerror.style.color = "Red";
+        loginemail.style.border = "2px solid red";
+        return false;
+      }
+
+}
+
+function loginValidatepwd(){
+
+    let regloginpwd = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+
+    if (regloginpwd.test(loginpwd.value)) {
+        loginpwderror.innerHTML = "";
+        return true;
+      }
+      else if (loginpwd.value.trim() == "") {
+        loginpwderror.innerHTML = "Not a valid password";
+        loginpwderror.style.color = "Red";
+        loginpwd.style.border = "2px solid red";
+        return false;
+      }
+       else {
+        return false;
+      }
+
+}
+
+function login(){
+    if((loginValidateemail()==true)&&(loginValidatepwd()==true)){
+        alert("Login successful");
+        location.reload();
+    }
+    else{
+
+        if(loginValidateemail()==false){
+            loginemail.style.border="2px solid red"
+        }
+        else if(loginValidatepwd()==false){
+            loginpwd.style.border = "2px solid red"
+        }
+
+       /* alert("Invalid username or password") */
+    }
+}
